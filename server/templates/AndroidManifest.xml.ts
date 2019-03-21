be327ca176df94d9d74e3174f6155b25a461ee7a
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-21 22:50:07 +0200

    templates

---
+++ server/templates/AndroidManifest.xml.ts
@@ -0,0 +1,33 @@
+export type info = {
+    package: string
+    versionCode: number
+    label: string
+}
+
+export function manifest(info: info) {
+    return `
+<?xml version='1.0'?>
+<manifest
+    xmlns:a='http://schemas.android.com/apk/res/android' 
+    package='${info.package}' 
+    a:versionCode='${info.versionCode}' 
+    a:versionName='0'>
+
+    <uses-permission a:name="android.permission.INTERNET"/>
+
+    <uses-sdk a:minSdkVersion="15"
+          a:targetSdkVersion="28"
+          a:maxSdkVersion="28" />
+
+    <application a:label='${info.label}' 
+        a:icon="@drawable/icon">
+        <activity a:name='${info.package}.MainActivity'>
+            <intent-filter>
+                <category a:name='android.intent.category.LAUNCHER'/>
+                <action a:name='android.intent.action.MAIN'/>
+            </intent-filter>
+        </activity>
+    </application>
+</manifest>
+`
+}
