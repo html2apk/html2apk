34edea49d7d09c825fc1209b3f8da807d2994123
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-23 00:14:01 +0200

    mkdir

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,17 @@ import Koa from 'koa'
 import Router from 'koa-router'
 import koaStatic from 'koa-static'
 import util from 'util'
-import url from 'url'
+import urlModule from 'url'
 import { manifest } from './templates/AndroidManifest.xml'
-const exec = util.promisify(require('child_process').exec)
+import { mainActivity } from './templates/MainActivity.java';
+import { layout } from './templates/activity_main.xml';
+import child_process from 'child_process'
+import fs from 'fs'
+import path from 'path'
+
+const mkdir = util.promisify(fs.mkdir)
+const writeFile = util.promisify(fs.writeFile)
+const exec = util.promisify(child_process.exec)
 
 const app = new Koa();
 const router = new Router()
@@ -12,14 +20,31 @@ const router = new Router()
 router.get('/apk/', async ctx => {
     try {
         const { stdout } = await exec('ls')
-        const uri = ctx.query.url
-        const hostname = url.parse(uri).hostname as string
-        ctx.type = 'text'
-        ctx.body = manifest({
+        const url = ctx.query.url
+        const hostname = urlModule.parse(url).hostname as string
+        const pkg = hostname.split('.').reverse()
+        const man = manifest({
             label: ctx.query.label
-            , package: hostname.split('.').reverse().join('.')
+            , package: pkg.join('.')
             , versionCode: ctx.query.versionCode
         })
+        const main = mainActivity(url)
+        const activityMain = layout()
+        const tmp = path.join('/tmp', Math.random().toString())
+        const src = path.join(tmp, 'src', ...pkg)
+        await mkdir(src, {
+            recursive: true
+        })
+        await writeFile(
+            path.join(tmp, 'AndroidManifest.xml')
+            , man
+        )
+        await writeFile(
+            path.join(src, 'MainActivity.java')
+            , mainActivity
+        )
+        ctx.type = 'text'
+        ctx.body = tmp
     } catch (err) {
         ctx.body = err
     }
