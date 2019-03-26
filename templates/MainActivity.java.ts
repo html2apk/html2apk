0c5213af0bfc479f28423187ac58c2e86cd72e9f
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-26 11:50:28 +0200

    progress dialog

--- templates/MainActivity.java.ts
+++ templates/MainActivity.java.ts
@@ -1,41 +1,51 @@
 export function mainActivity(pkg: string, url: string) {
     return `
-package ${pkg};
+    package ${pkg};
 
-import android.net.Uri;
-import android.app.Activity;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.content.Intent;
-import android.webkit.URLUtil;
-
-public class MainActivity extends Activity {
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
-
-      final WebView webView = (WebView) findViewById(R.id.webView);
-      final WebSettings settings = webView.getSettings();
-
-      settings.setJavaScriptEnabled(true);
-
-      webView.setWebViewClient(new WebViewClient() {
-         @Override
-         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-             if( URLUtil.isNetworkUrl(url) ) 
-                 return false;
-         
-                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                 startActivity( intent );
-             return true;
-         }
- 
-     });
-      webView.loadUrl("${url}");
-   }
-}
+    import android.app.ProgressDialog;
+    import android.net.Uri;
+    import android.app.Activity;
+    import android.os.Bundle;
+    import android.webkit.WebSettings;
+    import android.webkit.WebView;
+    import android.webkit.WebViewClient;
+    import android.content.Intent;
+    import android.webkit.URLUtil;
+    
+    public class MainActivity extends Activity {@Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+    
+            final WebView webView = (WebView) findViewById(R.id.webView);
+            final WebSettings settings = webView.getSettings();
+    
+            settings.setJavaScriptEnabled(true);
+    
+            final ProgressDialog progDailog = ProgressDialog.show(this, "Loading", "Please wait...", true);
+            progDailog.setCancelable(false);
+    
+            webView.setWebViewClient(new WebViewClient() {@Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    if (URLUtil.isNetworkUrl(url)) {
+    
+                        progDailog.show();
+                        view.loadUrl(url);
+                    }
+                    else {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    }
+                    return true;
+                }
+    
+                @Override
+                public void onPageFinished(WebView view, final String url) {
+                    progDailog.dismiss();
+                }
+            });
+            webView.loadUrl("${url}");
+        }
+    }
 `
 }
