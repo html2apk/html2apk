efef31370c18fc18c282f2e141e7d22acfdecc22
Gilad Kutiel <gilad.kutiel@gmail.com>
2019-03-27 10:20:48 +0200

    fix back

--- templates/MainActivity.java.ts
+++ templates/MainActivity.java.ts
@@ -2,27 +2,27 @@ export function mainActivity(pkg: string, url: string) {
     return `
     package ${pkg};
 
-    import android.app.ProgressDialog;
     import android.app.Activity;
+    import android.app.ProgressDialog;
     import android.content.Intent;
     import android.net.Uri;
     import android.os.Bundle;
+    import android.view.GestureDetector;
+    import android.view.GestureDetector.SimpleOnGestureListener;
+    import android.view.KeyEvent;
     import android.view.MotionEvent;
+    import android.view.View;
+    import android.view.View.OnKeyListener;
+    import android.view.View.OnTouchListener;
+    import android.webkit.URLUtil;
     import android.webkit.WebSettings;
     import android.webkit.WebView;
     import android.webkit.WebViewClient;
-    import android.webkit.URLUtil;
-    import android.view.View.OnTouchListener;
-    import android.view.View;
-    import android.view.GestureDetector;
-    import android.content.Context;
-    import android.view.GestureDetector.SimpleOnGestureListener;
-    
     
     public class MainActivity extends Activity {
     
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
     
@@ -35,48 +35,61 @@ export function mainActivity(pkg: string, url: string) {
             final ProgressDialog progDailog = ProgressDialog.show(this, "Loading", "Please wait...", true);
             progDailog.setCancelable(false);
     
-            final GestureDetector gd = new GestureDetector(this, new SimpleOnGestureListener(){
+            final GestureDetector gd = new GestureDetector(this, new SimpleOnGestureListener() {
                 @Override
-                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                public boolean onFling(final MotionEvent e1, final MotionEvent e2, final float velocityX,
+                        final float velocityY) {
                     try {
                         final float diffY = e2.getY() - e1.getY();
                         final int scroll = webView.getScrollY();
-                        
-                        if(scroll == 0 && diffY > 200){
+    
+                        if (scroll == 0 && diffY > 200)
                             webView.loadUrl(webView.getUrl());
-                        }
-                    } catch (Exception e) {}
+                    } catch (final Exception e) {}
     
                     return false;
                 }
             });
     
-            webView.setOnTouchListener(new OnTouchListener(){
+            webView.setOnKeyListener(new OnKeyListener() {
+    
+                @Override
+                public boolean onKey(final View view, final int keyCode, final KeyEvent e) {
+                    if (keyCode == KeyEvent.KEYCODE_BACK) {
+                        webView.goBack();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+    
+            webView.setOnTouchListener(new OnTouchListener() {
+                @Override
                 public boolean onTouch(final View view, final MotionEvent e) {
                     return gd.onTouchEvent(e);
                 }
             });
     
             webView.setWebViewClient(new WebViewClient() {
-                
+    
                 @Override
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                public void onPageFinished(final WebView view, final String url) {
+                    progDailog.dismiss();
+                }
+    
+                @Override
+                public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
                     if (URLUtil.isNetworkUrl(url)) {
                         progDailog.show();
                         view.loadUrl(url);
                     } else {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                         startActivity(intent);
                     }
                     return true;
                 }
-    
-                @Override
-                public void onPageFinished(WebView view, final String url) {
-                    progDailog.dismiss();
-                }
             });
-            
+    
             webView.loadUrl("${url}");
         }
     }
